bfed25a29a847ef4cd3c34156ca3ff6b98407add
KaanDalman1 <kaan.dalman@boun.edu.tr>
2023-11-01 18:13:29 +0300

    Update Codes.R

--- files/Codes.R
+++ files/Codes.R
@@ -81,4 +81,85 @@ ggplot(grouped_data, aes(x = price)) +
   labs(title = "Histogramm der Preise", x = "Preis", y = "Häufigkeit")
 
 
+#Task Moving Window Correlation 
+
+install.packages("zoo")
+install.packages("TTR")
+install.packages("tidyr")
+
+library(dplyr)
+library(zoo)
+library(ggplot2)
+library(TTR)
+library(tidyr)
+
+#data_A <- subset(data, short_name == "AEFES")
+#data_B <- subset(data, short_name == "AKBNK")
+
+#data_A <- data_A %>%
+ # group_by(year_month = format(as.Date(timestamp), "%Y-%m")) %>%
+  #summarize(avg_price = mean(price, na.rm = TRUE))
+#data_B <- data_B %>%
+ # group_by(year_month = format(as.Date(timestamp), "%Y-%m")) %>%
+  #summarize(avg_price = mean(price, na.rm = TRUE))
+
+#unternehmenA <- data[data$short_name == "AEFES", ]
+#unternehmenB <- data[data$short_name == "AKBNK", ]
+
+# Annahme: Du möchtest die Moving Window Correlation für die Unternehmen "Unternehmen1" und "Unternehmen2" durchführen.
+
+
+
+data_filtered <- data %>%
+  filter(short_name %in% c("AEFES", "AKBNK")) %>%
+  mutate(timestamp = as.POSIXct(timestamp, format = "%Y-%m-%dT%H:%M:%SZ")) %>%
+  arrange(short_name, timestamp) %>%
+  complete(timestamp = seq.POSIXt(min(timestamp), max(timestamp), by = "15 min")) %>%
+  tidyr::fill(price)  # Hier wird explizit angegeben, dass die Funktion aus dem "tidyr"-Paket stammt.
+
+
+# Gruppiere die Daten nach Timestamp und Unternehmen
+grouped_data <- data_filtered %>%
+  group_by(timestamp, short_name) %>%
+  summarise(price = mean(price, na.rm = TRUE)) %>%
+  ungroup()
+
+# Konvertiere die Timestamps in ein korrektes Format
+grouped_data$timestamp <- as.POSIXct(grouped_data$timestamp, format = "%Y-%m-%d %H:%M:%S")
+
+# Erstelle eine zoo-Zeitreihe
+zoo_data <- zoo(grouped_data$price, order.by = grouped_data$timestamp)
+
+
+# Definiere die Größe des Moving Windows in Sekunden
+window_size <- 10 * 3600  # 10 Stunden in Sekunden
+
+# Berechne die Moving Window Correlation für AEFES und AKBNK
+correlation <- rollapply(zoo_data, width = window_size, FUN = function(x) {
+  # Entferne fehlende Werte aus dem Fenster
+  x <- na.omit(x)
+  # Überprüfe, ob genügend Datenpunkte vorhanden sind
+  if (length(x) >= 2) {
+    cor(x["AEFES"], x["AKBNK"])
+  } else {
+    NA  # Wenn nicht genügend Daten vorhanden sind, gib NA zurück
+  }
+}, by = 900, align = "right")
+
+# Erstelle einen Dataframe mit den Ergebnissen der Moving Window Correlation
+correlation_df <- data.frame(timestamp = time(correlation), correlation = coredata(correlation))
+
+# Filtere Zeilen mit NA-Korrelationswerten aus
+correlation_df <- correlation_df[!is.na(correlation_df$correlation), ]
+
+# Erstelle einen Graphen
+ggplot(correlation_df, aes(x = timestamp, y = correlation)) +
+  geom_line() +
+  labs(title = "Moving Window Correlation zwischen AEFES und AKBNK",
+       x = "Zeitstempel",
+       y = "Korrelation") +
+  theme_minimal()
+
+
+
 
